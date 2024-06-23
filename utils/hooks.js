9f8d36832e6ef618ab66319b568256f2b910a37d
elpulga <phinguyenuit10@gmail.com>
2024-06-23 21:21:41 +0700

    fix login

--- utils/hooks.js
+++ utils/hooks.js
@@ -2,11 +2,10 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Toast } from "react-native-toast-message/lib/src/Toast";
 
-export const useMessageAndErrorUser = (
-  navigation,
-  dispatch,
-) => {
-  const { loading, message, error, isAuthenticated } = useSelector((state) => state.user);
+export const useMessageAndErrorUser = (navigation, dispatch, navigateTo) => {
+  const { loading, message, error, isAuthenticated } = useSelector(
+    (state) => state.user
+  );
 
   useEffect(() => {
     if (error) {
